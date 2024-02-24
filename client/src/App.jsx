0d4045fde23710277db2df5fd2efe8fe66d425ab
batkhishig09 <batkhishig0009@yahoo.com>
2024-02-24 16:35:59 -0600

    3rd

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,12 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
 import Projects from './pages/Project'
+import Header from './components/Header';
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Header />
      <Routes>
       <Route path="/" element={<Home/>}  />
       <Route path="about" element={<About/>}  />
