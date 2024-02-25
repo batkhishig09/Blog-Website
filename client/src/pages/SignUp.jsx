41f939103ae4ad25f8c9d5e5112d7220a9119491
batkhishig09 <batkhishig0009@yahoo.com>
2024-02-25 12:58:37 -0600

    hello

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,9 +1,45 @@
 import React from 'react'
+import {Button, Label, TextInput} from 'flowbite-react'
+import {Link} from 'react-router-dom'
 
 const SignUp = () => {
   return (
-    <div>
-      Signup
+    <div className='min-h-screen mt-20 '>
+      <div className='flex  p-3 max-w-3xl mx-auto  md:flex-row md:items-center'>
+        <div className=''>
+          <form className='flex flex-col gap-5 '>
+            <div>
+              <Label value="Your Username"/>
+              <TextInput
+              type='text'
+              placeholder='Username'
+              id='username'
+              />
+            </div>
+            <div>
+              <Label value="Your Email"/>
+              <TextInput
+              type='text'
+              placeholder='Email'
+              id='email'
+              />
+            </div>
+            <div>
+              <Label value="Your Password"/>
+              <TextInput
+              type='password'
+              placeholder='Password'
+              id='password'
+              />
+            </div>
+            <Button gradientDuoTone='purpleToPink' type='submit'>Sign Up</Button>
+          </form>
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>Have an account?</span>
+            <Link to='/signin' className='text-blue-500'>Sign In</Link>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- server/api/index.js
+++ server/api/index.js
@@ -1,7 +1,36 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js'
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => { 
+     console.log('MongoDB is connected')
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on 3000')
+});
+
+app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = error.message || "internal Server Error";
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message
+    })
 });
